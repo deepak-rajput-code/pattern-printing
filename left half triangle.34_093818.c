a58a252dca1a62d4821c39ba84cad6da03738ad6
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 22:43:39 +0530

    Add files via upload

---
+++ left half triangle.34_093818.c
@@ -0,0 +1,20 @@
+      #include<stdio.h>
+      int main(){
+          int n;
+          printf("enter the number n:");
+          scanf("%d",&n);
+          
+          int i;
+          for(i=1;i<=n;i++){
+              int s;
+              for(s=1;s<=n-i;s++){
+                  printf("   ");
+              }
+              int j;
+              for(j=1;j<=i;j++){
+                  printf(" * ");
+              }
+              printf("\n");
+          }
+          return 0;
+      }
