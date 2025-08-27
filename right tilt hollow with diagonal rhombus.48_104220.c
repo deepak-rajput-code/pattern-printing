243f9e268deabe15c504a0f5216816a0a0d2ed2a
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 23:10:39 +0530

    Add files via upload

---
+++ right tilt hollow with diagonal rhombus.48_104220.c
@@ -0,0 +1,26 @@
+        #include<stdio.h>
+     int main(){
+         int n;
+         printf("enter the number n:");
+         scanf("%d",&n);
+         
+         int i;
+         for(i=1;i<=n;i++){
+             int s;
+             for(s=1;s<=n-i;s++){
+                 printf(" ");
+             }
+             int j;
+             for(j=1;j<=n;j++){
+              if(i==1||j==1||j==n||i==n ||i==j||j==n-i+1){ //i==j and j==n-i+1 for daigonal
+                  printf("*");
+              }
+              else{
+                  printf(" ");
+              }
+              
+             }
+             printf("\n");
+         }
+         return 0;
+     }
