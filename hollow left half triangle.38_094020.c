0cb4eced253907d40f0d1842bf7af936f4880a5c
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 22:38:36 +0530

    Add files via upload

---
+++ hollow left half triangle.38_094020.c
@@ -0,0 +1,27 @@
+     #include<stdio.h>
+      int main(){
+          int n;
+          printf("enter the number n:");
+          scanf("%d",&n);
+          
+          int i;
+          for(i=1;i<=n;i++){
+              
+              int s;
+              for(s=1;s<=n-i;s++){
+                  printf(" ");
+              }
+               int j;
+               for(j=1;j<=i;j++){
+                   if(j ==1 || i==n || j==i){
+                       printf("*");
+                   }
+                   else{
+                       printf(" ");
+                   }
+               }
+                   
+             printf("\n");
+          }
+          return 0;
+      }
