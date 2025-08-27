a8c826c4863da1f73129468f9ca610aa6b092729
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 22:36:28 +0530

    Add files via upload

---
+++ hollow daimond.52_104438.c
@@ -0,0 +1,53 @@
+        #include<stdio.h>
+     int main(){
+         int n;
+         printf("enter the odd number n:");
+         scanf("%d",&n);
+         
+         int i;
+         for(i=1;i<=n/2;i++){
+             int j;
+             for(j=1;j<=n/2+1-i;j++){
+                 printf(" ");
+             }
+             int k;
+             for(k=1;k<=2*i-1;k++){
+                  if(k==1||k==2*i-1){
+                      printf("*");
+                  }
+                  else{
+                      printf(" ");
+                  }
+             }
+             printf("\n");
+         }
+         
+         int x;
+         for(x=1;x<=n;x++){
+          if(x==1||x==n){
+              printf("*");
+          }
+          else{
+              printf(" ");
+          }
+         }
+         printf("\n");
+         int r;
+         for(r=1;r<=n/2;r++){
+             int y;
+             for(y=1;y<=r;y++){
+             printf(" ");
+         }
+         int h;
+         for(h=1;h<=n-2*r ;h++){
+             if(h==1||h==n-2*r){
+                 printf("*");
+             }
+             else{
+                 printf(" ");
+             }
+         }
+         printf("\n");
+         }
+         return 0;
+     }
