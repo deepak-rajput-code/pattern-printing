4d676c4ac511c72e1aa2d8e65af85d0d742daf89
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 22:35:34 +0530

    Add files via upload

---
+++ daigonal of hollow sqaure.46_104115.c
@@ -0,0 +1,21 @@
+          #include<stdio.h>
+     int main(){
+         int n;
+         printf("enter the number n:");
+         scanf("%d",&n);
+         
+         int i;
+         for(i=1;i<=n;i++){
+             int j;
+             for(j=1;j<=n;j++){
+              if(i==1||j==1||i==n||j==n || i==j||j==n-i+1){
+                  printf(" * ");
+              }
+              else{
+                  printf("   ");
+              }
+             }
+             printf("\n");
+         }
+         return 0;
+     }
