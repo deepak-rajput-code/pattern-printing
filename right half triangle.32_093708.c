2e341a9711cf5a3fd0160c15440c827a2230f1c7
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 23:10:15 +0530

    Add files via upload

---
+++ right half triangle.32_093708.c
@@ -0,0 +1,17 @@
+   // right half triangle
+     #include<stdio.h>
+       int main(){
+           int n;
+           printf("enter the number n:");  //here n is number of rows
+           scanf("%d",&n);
+           
+           int i;
+           for(i=1;i<=n;i++){
+               int j;
+               for(j=1;j<=i;j++){
+                   printf(" * ");
+               }
+               printf("\n");
+           }
+           return 0;
+       }
