61ec9fe3d442f67c1bc0daff1b5e09ff98e707fe
DEEPAK RAJPUT <deepakrajput4083@gmail.com>
2025-08-27 22:34:43 +0530

    Add files via upload

---
+++ alphabet full triangle.55_085650.c
@@ -0,0 +1,23 @@
+ #include<stdio.h>
+    int main(){
+        int n;
+        printf("enter the number n:");
+        scanf("%d",&n);
+        int i;
+        for(i=1;i<=n;i++){
+            int s;
+            for(s=1;s<=n-i;s++){
+            printf(" ");
+         }
+         int j;
+         for(j=1;j<= 2*i-1;j++){
+             int d;
+             d=j+64;
+             char ch;
+             ch='d';
+             printf("%c", d);
+         }
+         printf("\n");
+        }
+        return 0;
+    }
